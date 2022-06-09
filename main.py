0ff2b0181812190cf0a6663043333d3f1d849d02
Roman <roma-xom@mail.ru>
2022-06-09 18:15:10 +0600

    Add logic for bot

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ import telebot
 from flask import Flask, request
 
 TOKEN = '5572856864:AAF8OKd8cSUuZb9YGxwyRz8SxhvF6ghysKE'
-APP_URL = f'https://virtualjaheroku.herokuapp.com/{TOKEN}'
+APP_URL = f'https://vitaljaheroku.herokuapp.com/{TOKEN}'
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.from_user.id, 'Начнем работу, ' + message.from_user.first_name)
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo(message):
-    bot.send_message(message.from_user.id, message.text)
+    bot.reply_to(message, message.text)
 
 
-@server.route('/'+TOKEN, methods=['POST'])
+@server.route('/' + TOKEN, methods=['POST'])
 def get_message():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
@@ -34,4 +34,4 @@ def webhook():
 
 
 if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=int(os.environ.get('PORT')))
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
